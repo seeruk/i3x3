b7bfa062a2a71e3ed3394ebdb01d36034e428280
Elliot Wright <wright.elliot@gmail.com>
2017-05-16 23:05:55 +0100

    Well, it works.

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
 	"fmt"
 	"math"
+	"os"
+	"os/exec"
 )
 
 type Direction string
@@ -25,13 +29,13 @@ const (
 // These come from i3
 var (
 	// Current workspace
-	cw float64 = 18
+	cw float64
 	// Current output
-	co float64 = 2
+	co float64
 	// Number of outputs
-	no float64 = 2
+	no float64
 	// Direction (i.e. "up", "down", "left", "right")
-	dir = Up
+	dir = Down
 )
 
 type EdgeFunc func(tar float64) bool
@@ -68,8 +72,59 @@ var targetFuncs = map[Direction]TargetFunc{
 	},
 }
 
+type Rect struct {
+	X      int `json:"x"`
+	Y      int `json:"y"`
+	Width  int `json:"width"`
+	Height int `json:"height"`
+}
+
+type Output struct {
+	Name             string `json:"name"`
+	Active           bool   `json:"active"`
+	Primary          bool   `json:"primary"`
+	Rect             Rect   `json:"rect"`
+	CurrentWorkspace string `json:"current_workspace"`
+}
+
+type Workspace struct {
+	Num     int    `json:"num"`
+	Name    string `json:"name"`
+	Visible bool   `json:"visible"`
+	Focused bool   `json:"focused"`
+	Rect    Rect   `json:"rect"`
+	Output  string `json:"output"`
+	Urgent  bool   `json:"urgent"`
+}
+
 func main() {
+	var move bool
+	var sdir string
+
+	flag.BoolVar(&move, "move", false, "Whether or not to move the focused container too")
+	flag.StringVar(&sdir, "direction", "down", "The direction to move in (up, down, left, right)")
+	flag.Parse()
+
+	dir = Direction(sdir)
+
+	outputs, err := getOutputs()
+	if err != nil {
+		panic(err)
+	}
+
+	workspaces, err := getWorkspaces()
+	if err != nil {
+		panic(err)
+	}
+
+	// Setup i3 values...
+	no = getActiveOutputCount(outputs)
+	cw = getCurrentWorkspaceNum(workspaces)
+	co = getCurrentOutputNum(cw, no)
+
+	fmt.Println(no)
 	fmt.Println(cw)
+	fmt.Println(co)
 
 	targetFn, ok := targetFuncs[dir]
 	if !ok {
@@ -82,10 +137,87 @@ func main() {
 	}
 
 	if edgeFn(cw) {
-		panic("Already at edge")
+		fmt.Println("Already at edge...")
+		os.Exit(0)
 	}
 
 	target := targetFn()
 
-	fmt.Printf("Moving %v from %v to %v\n", dir, cw, target)
+	if move {
+		moveToWorkspace(target)
+	}
+
+	switchToWorkspace(target)
+}
+
+func moveToWorkspace(workspace float64) error {
+	return exec.Command("i3-msg", "move", "container", "to", "workspace", fmt.Sprintf("%v", workspace)).Run()
+}
+
+func switchToWorkspace(workspace float64) error {
+	return exec.Command("i3-msg", "workspace", fmt.Sprintf("%v", workspace)).Run()
+}
+
+func getOutputs() ([]Output, error) {
+	var outputs []Output
+
+	out, err := exec.Command("i3-msg", "-t", "get_outputs").Output()
+	if err != nil {
+		return []Output{}, err
+	}
+
+	err = json.Unmarshal(out, &outputs)
+	if err != nil {
+		return []Output{}, err
+	}
+
+	return outputs, nil
+}
+
+func getWorkspaces() ([]Workspace, error) {
+	var workspaces []Workspace
+
+	out, err := exec.Command("i3-msg", "-t", "get_workspaces").Output()
+	if err != nil {
+		return []Workspace{}, err
+	}
+
+	err = json.Unmarshal(out, &workspaces)
+	if err != nil {
+		return []Workspace{}, err
+	}
+
+	return workspaces, nil
+}
+
+func getActiveOutputCount(os []Output) float64 {
+	var aos []Output
+
+	for _, o := range os {
+		if o.Active {
+			aos = append(aos, o)
+		}
+	}
+
+	return float64(len(aos))
+}
+
+func getCurrentOutputNum(workspace float64, outputs float64) float64 {
+	mod := math.Mod(workspace, outputs)
+
+	if mod == 0 {
+		return outputs
+	}
+
+	return mod
+}
+
+func getCurrentWorkspaceNum(workspaces []Workspace) float64 {
+	for _, workspace := range workspaces {
+		if workspace.Focused {
+			return float64(workspace.Num)
+		}
+	}
+
+	return 1.0
 }
