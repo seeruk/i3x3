678db79b7ca991407af0df6dced6d7aa8942eaad
Elliot Wright <wright.elliot@gmail.com>
2017-08-11 21:34:17 +0100

    Well, it works.

--- main.go
+++ main.go
@@ -8,6 +8,11 @@ import (
 	"os"
 	"os/exec"
 	"strconv"
+
+	"time"
+
+	"github.com/mattn/go-gtk/gdk"
+	"github.com/mattn/go-gtk/gtk"
 )
 
 // A direction represents the course that will be taken for movement through workspaces.
@@ -166,11 +171,82 @@ func main() {
 
 	target := targetFn()
 
+	// Try create the WS grid preview. This should be created in another thread so we can still send
+	// the i3-msg commands as quickly as possible.
+	go func() {
+		gtk.Init(&os.Args)
+
+		// Create the window
+		window := gtk.NewWindow(gtk.WINDOW_POPUP)
+		window.SetAcceptFocus(false)
+		window.SetDecorated(false)
+		window.SetKeepAbove(true)
+		window.SetPosition(gtk.WIN_POS_CENTER_ALWAYS)
+		window.SetResizable(false)
+		window.SetSkipTaskbarHint(true)
+		window.SetTitle("i3x3 GTK WSS")
+		window.SetTypeHint(gdk.WINDOW_TYPE_HINT_NOTIFICATION)
+		window.Stick()
+		window.Connect("destroy", gtk.MainQuit)
+
+		// Set main colours
+		window.ModifyBG(gtk.STATE_NORMAL, gdk.NewColor("#1A1A1A"))
+		window.ModifyFG(gtk.STATE_NORMAL, gdk.NewColor("#D3D3D3"))
+
+		table := gtk.NewTable(uint(ix), uint(iy), false)
+		table.SetBorderWidth(3)
+
+		labelCount := ix * iy
+
+		for i := 0; i < labelCount; i++ {
+			ico := int(co)
+			ino := int(no)
+
+			ws := ico + (ino * i)
+
+			label := gtk.NewLabel("")
+			label.SetSizeRequest(60, 60)
+
+			if int(target) == ws {
+				// Highlight the active workspace
+				label.ModifyBG(gtk.STATE_NORMAL, gdk.NewColor("#2A2A2A"))
+				label.ModifyFG(gtk.STATE_NORMAL, gdk.NewColor("white"))
+				label.SetMarkup(fmt.Sprintf("<b>%d</b>", int(ws)))
+			} else {
+				label.SetMarkup(fmt.Sprintf("%d", int(ws)))
+			}
+
+			row := i / ix
+			col := i - (row * ix)
+
+			urow := uint(row)
+			ucol := uint(col)
+
+			// Attach it to the correct place in the table
+			table.Attach(label, ucol, ucol+1, urow, urow+1, gtk.EXPAND, gtk.EXPAND, 0, 0)
+		}
+
+		window.Add(table)
+		window.ShowAll()
+
+		gtk.Main()
+	}()
+
 	if move {
-		moveToWorkspace(target)
+		err := moveToWorkspace(target)
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	switchToWorkspace(target)
+	err = switchToWorkspace(target)
+	if err != nil {
+		panic(err)
+	}
+
+	time.Sleep(500 * time.Millisecond)
+
+	gtk.MainQuit()
 }
 
 // fatal panics if the given error is not nil.
