c3da7399ddf913f2dd0797ad0ed6fee3d1716420
Elliot Wright <wright.elliot@gmail.com>
2017-11-11 22:36:58 +0000

    Quite a lot of changes. Big changes to the protobuf protocol. New daemon server. New way of generating the protocol too. Removed some unused stuff. Removed some stuff still in use technically.

--- cmd/i3x3d/main.go
+++ cmd/i3x3d/main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/SeerUK/i3x3/pkg/daemon"
+	"github.com/SeerUK/i3x3/pkg/proto"
+)
+
+const (
+	DaemonPort uint16 = 44044
 )
 
 // i3x3d is an optional daemon used to perform non-critical tasks for i3x3. It is daemonised to
 // improve performance, and to gather information about the environment at startup.
 //
 // The functionality currently includes:
+// * Message handling:
+//   Processing messages from i3x3ctl, sending commands to i3 as necessary to switch workspaces, or
+//   move containers too.
 // * Automatic workspace redistribution:
 //   When you change your display configuration (i.e. remove an output from X, or add one, etc.),
 //   i3x3d will detect this change, and automatically redistribute i3's workspaces in a way that
@@ -19,16 +30,23 @@ import (
 //   After initially building this into the i3x3ctl command, performance became an issue. Having the
 //   overlay in i3x3d means GTK can start up and be initialised, leaving as little work as possible
 //   left to do when we want the overlay to be shown. The result is a much more responsive overlay.
-//
-// No other functionality is planned at present.
 
 func main() {
+	ctx, cfn := context.WithCancel(context.Background())
+
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt, os.Kill)
 
-	// @TODO: Port i3x3fixd and i3x3overlayd code into i3x3d.
+	commands := make(chan proto.DaemonCommand)
+
+	server := daemon.NewServer(DaemonPort, commands)
+	serverDone := daemon.NewServerBackgroundThread(ctx, server)
 
 	sig := <-signals
-
 	log.Printf("caught signal: %v\n", sig)
+
+	cfn()
+
+	// Wait for our background threads to clean up.
+	<-serverDone
 }
