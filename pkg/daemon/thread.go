9bb7b2de82c5c81eacd3bdbaff9f37c452b3ec67
Elliot Wright <wright.elliot@gmail.com>
2017-11-13 21:23:16 +0000

    Updated a bunch of comments. Updated RPC thread to use a mutex, and unassign the server on stop.

--- pkg/daemon/thread.go
+++ pkg/daemon/thread.go
@@ -6,7 +6,9 @@ import "context"
 // necessarily run in the background, and it may not necessarily have to be stopped by calling stop
 // (i.e. it could end on it's own).
 type Thread interface {
+	// Start starts some work. The work only end when Stop is called, or it may end on it's own.
 	Start() error
+	// Stop attempts to stop a started thread.
 	Stop() error
 }
 
@@ -24,12 +26,14 @@ func NewBackgroundThread(ctx context.Context, thread Thread) <-chan BackgroundTh
 	bail := make(chan struct{}, 1)
 	done := make(chan BackgroundThreadResult, 1)
 
+	// Wait for a request to stop from the context, in the background.
 	go func() {
 		// First, wait for some kind of signal to do anything.
 		select {
 		case <-ctx.Done():
 		case <-bail:
-			// We should only reach this return if there was an error starting the thread.
+			// If we hit here, it means that the thread stopped on it's own. This might mean there
+			// was an error.
 			return
 		}
 
@@ -37,27 +41,35 @@ func NewBackgroundThread(ctx context.Context, thread Thread) <-chan BackgroundTh
 		// has finished. Otherwise there's no point in using it...
 		err := thread.Stop()
 
-		// Once we stop, we need to tell the other routine to not try to send more results.
+		// Once we stop, we need to tell the other goroutine to not try to send more results. Then
+		// we send our result from stopping.
 		bail <- struct{}{}
 		done <- BackgroundThreadResult{
 			Error: err,
 		}
 	}()
 
+	// Start the thread, and wait for it to stop, or be stopped.
 	go func() {
 		// This call should block. Once this stops, we will check to see if we requested a shutdown.
+		// If we didn't (i.e. if the other thread hasn't told us to bail) then this thread must have
+		// stopped on it's own (maybe an error).
 		err := thread.Start()
 
 		select {
 		case <-bail:
-			return // If a shutdown has already been requested.
+			// If a stop was requested, we will have already sent the result (the result from
+			// stopping), therefore, we shouldn't send another result
+			return
 		default:
 		}
 
-		// If we didn't request a shutdown, we will want to stop the goroutine above from waiting
-		// for something to tell it to shutdown (i.e. the context). Then, we'll send a response that
-		// may contain an error (there may not always be an error).
+		// If the Thread didn't stop because it was told to (but rather because it finished for
+		// whatever reason), then we need to tell the other goroutine to bail, as there's no point
+		// in it trying to tell a Thread that's already stopped to stop.
 		bail <- struct{}{}
+
+		// Once we've told the other thread to bail, send the result of the Thread's start call.
 		done <- BackgroundThreadResult{
 			Error: err,
 		}
