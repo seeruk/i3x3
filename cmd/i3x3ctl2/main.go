54044c6b195b26fa5b07c22691fc4ee04ffc3186
Elliot Wright <wright.elliot@gmail.com>
2017-11-23 22:38:51 +0000

    Implemented basic messy client for testing new daemon with.

---
+++ cmd/i3x3ctl2/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/SeerUK/i3x3/pkg/daemon"
+	"github.com/SeerUK/i3x3/pkg/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	var direction string
+	var disableOverlay bool
+	var move bool
+
+	flag.BoolVar(&move, "move", false, "Whether or not to move the focused container too")
+	flag.StringVar(&direction, "direction", "down", "The direction to move in (up, down, left, right)")
+	flag.BoolVar(&disableOverlay, "no-overlay", false, "Used to disable the GTK-based overlay")
+	flag.Parse()
+
+	ctx, cfn := context.WithTimeout(context.Background(), time.Second)
+	defer cfn()
+
+	conn, err := grpc.DialContext(ctx, fmt.Sprintf("127.0.0.1:%v", daemon.RPCPort), grpc.WithInsecure())
+	fatal(err)
+
+	defer conn.Close()
+
+	client := proto.NewDaemonServiceClient(conn)
+
+	resp, err := client.HandleCommand(ctx, &proto.DaemonCommand{
+		Direction: direction,
+		Overlay:   !disableOverlay,
+		Move:      move,
+	})
+
+	fatal(err)
+
+	fmt.Println(resp.Target)
+}
+
+// fatal panics if the given error is not nil.
+func fatal(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
